e4fcb91a4415e547a0bb85cd659436b1007deb9b
Renato Contaifer <renatocontaifer@gmail.com>
2016-01-19 15:14:16 -0800

    Iniciando repo

--- src-js/fastclick.js
+++ src-js/fastclick.js
@@ -826,35 +826,3 @@
 		window.FastClick = FastClick;
 	}
 }());
-
-$(document).ready(function(){
-
-  $('.menu-anchor').on('click', function() {
-    $('.full-container').toggleClass('open');
-  });
-
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        var alturaMenu = $('.nav-bar').height();
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 600);
-        return false;
-      }
-    }
-
-  });
-
-  $('.select-area .select').change(function(){
-    $text = $(this).find('option:selected').text();
-    $(this).parent().find('.select-view p').text($text);
-  });
-
-});
-
-  window.addEventListener('load', function() {
-    new FastClick(document.body);
-}, false);
